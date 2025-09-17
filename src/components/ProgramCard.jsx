99cb3c33c337f5c54860edc8163000534305317e
Lucasdeekay <lucasdeekay98@gmail.com>
2025-09-17 19:45:13 +0100

    Updated program card

--- src/components/ProgramCard.jsx
+++ src/components/ProgramCard.jsx
@@ -1,7 +1,12 @@
+import { NavLink as RouterNavLink } from "react-router-dom";
+
 export const ProgramCard = ({ icon, title, description }) => (
-  <div className="p-6 bg-white rounded-2xl shadow-lg hover:shadow-xl transition-shadow duration-300 transform hover:-translate-y-2 text-center">
-    <div className="p-4 rounded-full bg-gray-100 inline-block mb-4">{icon}</div>
-    <h3 className="text-xl font-bold mb-2">{title}</h3>
-    <p className="text-gray-600 text-sm">{description}</p>
-  </div>
+    <RouterNavLink to="/application" className="p-6 bg-white rounded-2xl shadow-lg hover:shadow-xl transition-shadow duration-300 transform hover:-translate-y-2 text-center">
+        <div className="p-4 rounded-full bg-gray-100 inline-block mb-4">
+          {icon}
+        </div>
+        <h3 className="text-xl font-bold mb-2">{title}</h3>
+        <p className="text-gray-600 text-sm">{description}</p>
+    </RouterNavLink>
+
 );
